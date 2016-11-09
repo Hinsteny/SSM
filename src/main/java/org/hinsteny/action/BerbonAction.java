3b49741b5328e6124bbdaad46e22b17a0ec08316
hinsteny@qq.com <hinsteny@qq.com>
2016-11-09 15:41:17 +0800

    # 添加易宝网银测试

--- src/main/java/org/hinsteny/action/BerbonAction.java
+++ src/main/java/org/hinsteny/action/BerbonAction.java
@@ -1,6 +1,5 @@
 package org.hinsteny.action;
 
-import com.hisoka.rest.Get;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -40,6 +39,19 @@ public class BerbonAction {
 		request.getParameterMap();
 		result.putAll(request.getParameterMap());
 		System.out.println(result);
+	}
+
+	@RequestMapping("/yepay/Gw/pay")
+	@ResponseBody
+	public void yeGwPay(HttpServletRequest request, HttpServletResponse response, Model model) {
+		String requestr = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=GBK\"/></head><body><form name = \"pay_form\" action=\"https://www.yeepay.com/app-merchant-proxy/node\" method=\"post\"><input type=\"hidden\" name=\"pa_MP\" id=\"pa_MP\" value=\"Goods Mp\"/><input type=\"hidden\" name=\"p8_Url\" id=\"p8_Url\" value=\"http://cashier.berbon.com/callback/return/012016110909582800001201258834\"/><input type=\"hidden\" name=\"pd_FrpId\" id=\"pd_FrpId\" value=\"ICBC-NET-B2C\"/><input type=\"hidden\" name=\"p2_Order\" id=\"p2_Order\" value=\"012016110909582800001201258834\"/><input type=\"hidden\" name=\"p6_Pcat\" id=\"p6_Pcat\" value=\"Goods kind\"/><input type=\"hidden\" name=\"p3_Amt\" id=\"p3_Amt\" value=\"0.1\"/><input type=\"hidden\" name=\"pm_Period\" id=\"pm_Period\" value=\"60\"/><input type=\"hidden\" name=\"p5_Pid\" id=\"p5_Pid\" value=\"GoodsName\"/><input type=\"hidden\" name=\"pn_Unit\" id=\"pn_Unit\" value=\"minute\"/><input type=\"hidden\" name=\"hmac\" id=\"hmac\" value=\"1678755c10cd06e060e8d8db41be78f0\"/><input type=\"hidden\" name=\"p0_Cmd\" id=\"p0_Cmd\" value=\"Buy\"/><input type=\"hidden\" name=\"p7_Pdesc\" id=\"p7_Pdesc\" value=\"Good describtion\"/><input type=\"hidden\" name=\"p1_MerId\" id=\"p1_MerId\" value=\"10012423519\"/><input type=\"hidden\" name=\"p4_Cur\" id=\"p4_Cur\" value=\"CNY\"/><input type=\"hidden\" name=\"p9_SAF\" id=\"p9_SAF\" value=\"0\"/></form></body><script type=\"text/javascript\">document.forms['pay_form'].submit();</script></html>";
+		response.setCharacterEncoding("UTF-8");
+		try {
+			response.getWriter().println(requestr);
+		} catch (Exception e){
+			System.out.println(e.toString());
+		}
+
 	}
 
 	@RequestMapping("/yepay/Gw/inform")

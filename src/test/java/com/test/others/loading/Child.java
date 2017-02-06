1388293692f6164fb524b68bc2f15864bf0dfaba
hinsteny@qq.com <hinsteny@qq.com>
2017-02-06 09:29:29 +0800

    # class loader to interface and parent class if be loaded

---
+++ src/test/java/com/test/others/loading/Child.java
@@ -0,0 +1,12 @@
+package com.test.others.loading;
+
+/**
+ * @author Hinsteny
+ * @Describtion
+ * @date 2017/1/20
+ * @copyright: 2016 All rights reserved.
+ */
+public class Child extends Parent {
+
+
+}
